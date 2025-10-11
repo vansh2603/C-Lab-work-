c1043c4e84d8b77ff983541f9eaf92d8ebdad0e4
Vansh Menia <2025a1r123@mietjammu.in>
2025-10-11 13:07:24 +0530

    Update largest no.c

--- largest no.c
+++ largest no.c
@@ -4,12 +4,15 @@ int main() {
     printf("Enter three numbers: ");
     scanf("%d %d %d", &a, &b, &c);
 
-    if (a >= b && a >= c)
+    if (a >= b && a >= c){
         printf("%d is the largest", a);
-    else if (b >= a && b >= c)
+    }
+    else if (b >= a && b >= c){
         printf("%d is the largest", b);
-    else
+    }    
+    else{
         printf("%d is the largest", c);
+    }  
 
     return 0;
 }
